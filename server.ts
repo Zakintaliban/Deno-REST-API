3ea3fc90276ce20243999eb05b02b279124d6c33
Khasbullah Nukman Zakin <nukmans23@gmail.com>
2020-06-13 15:38:27 +0700

    updating server.ts

--- server.ts
+++ server.ts
@@ -1,15 +1,11 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application } from "https://deno.land/x/oak/mod.ts";
+import router from "./routes.ts";
 
 const port = 5000;
 const app = new Application();
-const router = new Router();
 
 app.use(router.routes());
 app.use(router.allowedMethods());
-
-router.get("/api", ({ response }: { response: any }) => {
-  response.body = "Selamat datang, Deno!";
-});
 
 console.log(`server running on port ${port}`);
 
